48665f8e4f8bbdd2b3f51151731f3293cf0e9746
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:10 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,32 +1,32 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'maintenance'
-    MODULE_NAME = _('Maintenance & CMMS')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'build-outline'
-    MODULE_DESCRIPTION = _('Preventive and corrective maintenance for assets and equipment')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'operations'
+MODULE_ID = 'maintenance'
+MODULE_NAME = _('Maintenance & CMMS')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'build-outline'
+MODULE_DESCRIPTION = _('Preventive and corrective maintenance for assets and equipment')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'operations'
 
-    MENU = {
-        'label': _('Maintenance & CMMS'),
-        'icon': 'build-outline',
-        'order': 57,
-    }
+MENU = {
+    'label': _('Maintenance & CMMS'),
+    'icon': 'build-outline',
+    'order': 57,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Work Orders'), 'icon': 'build-outline', 'id': 'work_orders'},
 {'label': _('Schedules'), 'icon': 'calendar-outline', 'id': 'schedules'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'maintenance.view_maintenanceorder',
+PERMISSIONS = [
+    'maintenance.view_maintenanceorder',
 'maintenance.add_maintenanceorder',
 'maintenance.change_maintenanceorder',
 'maintenance.delete_maintenanceorder',
 'maintenance.manage_settings',
-    ]
+]
