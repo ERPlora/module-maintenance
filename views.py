bcafc4e3215afe39aaca1e35499dcef5443e0d2e
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-03-07 13:18:22 +0100

    feat: convert drawer panels to full-page forms

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ Maintenance & CMMS Module Views
 """
 from django.core.paginator import Paginator
 from django.db.models import Q, Count
+from django.http import HttpResponse
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render as django_render
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -113,6 +115,7 @@ def maintenance_orders_list(request):
     }
 
 @login_required
+@htmx_view('maintenance/pages/maintenance_order_add.html', 'maintenance/partials/maintenance_order_add_content.html')
 def maintenance_order_add(request):
     hub_id = request.session.get('hub_id')
     if request.method == 'POST':
@@ -140,10 +143,13 @@ def maintenance_order_add(request):
         obj.cost = cost
         obj.notes = notes
         obj.save()
-        return _render_maintenance_orders_list(request, hub_id)
-    return django_render(request, 'maintenance/partials/panel_maintenance_order_add.html', {})
+        response = HttpResponse(status=204)
+        response['HX-Redirect'] = reverse('maintenance:maintenance_orders_list')
+        return response
+    return {}
 
 @login_required
+@htmx_view('maintenance/pages/maintenance_order_edit.html', 'maintenance/partials/maintenance_order_edit_content.html')
 def maintenance_order_edit(request, pk):
     hub_id = request.session.get('hub_id')
     obj = get_object_or_404(MaintenanceOrder, pk=pk, hub_id=hub_id, is_deleted=False)
@@ -161,7 +167,7 @@ def maintenance_order_edit(request, pk):
         obj.notes = request.POST.get('notes', '').strip()
         obj.save()
         return _render_maintenance_orders_list(request, hub_id)
-    return django_render(request, 'maintenance/partials/panel_maintenance_order_edit.html', {'obj': obj})
+    return {'obj': obj}
 
 @login_required
 @require_POST
